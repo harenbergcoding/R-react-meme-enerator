4e7c70d8e39f2fecaddf0e77fc4a4ae6d7cd0ce1
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-14 16:41:19 +0200

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const getImage = () => {
+    axios
+      .get('https://api.memegen.link/templates/')
+      .then((res) => {
+        console.log(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(getImage);
+      });
+  };
+  console.log(getImage);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={getImage}>Get Image</button>
+      </div>
+      <h1>React Meme Generator</h1>
+      <br />
+      <div
+        className="memebox"
+        style={{
+          width: 350,
+          height: 350,
+          margin: '0 auto',
+          border: '1px solid black',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Hi
+      </div>
+      <br />
+      <label>
+        Top Text <input />
+      </label>
+      <br />
+      <label>
+        Bottom Text <input />
+      </label>
+      <br /> <br />
+      Meme template <input /> <button>Submit</button>
+      <br />
+      <br />
+      <button>Download</button>
     </div>
   );
 }
-
 export default App;
